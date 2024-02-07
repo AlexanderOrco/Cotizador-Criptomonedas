0261ece44a81586dd05d18f2571db2c46c9c7e62
AlexanderOrco <orcoalexander@gmail.com>
2024-02-06 22:42:00 -0300

    first commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    { id: "USD", nombre: "Dolar de Estados Unidos" },
+    { id: "ARS", nombre: "Peso Argentino" },
+    { id: "UER", nombre: "Euro" },
+    { id: "GBP", nombre: "Libra Esterlina" },
+];
+
+export {
+    monedas
+}
